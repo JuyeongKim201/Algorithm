c12f107cb51c5169040e5fdbeab6cd7c39d9b911
JuyeongKim201 <juyeong.kim.201@gmail.com>
2023-10-29 16:05:05 +0900

    231029 1st commit

--- 8_DP/boj9251_LCS.py
+++ 8_DP/boj9251_LCS.py
@@ -1,42 +1,46 @@
+# 문제: LCS (골드 5)
 '''
-[기본 아이디어]
-문자열 A, B가 주어졌을 때, (i = index)
-CS 배열을 만듦 -> CS = [0]*(number of i)
+각 i와 j에 대해서, dp[i][j]는 str1과 str2 전체의 LCS를 구하는 것의 하위 부분 문제들, 
+즉 str1[i-1]와 str2[j-1]의 LCS를 구하는 문제인 셈. 
 
-if A[i] == B[i]:
-    if i == 0:
-        CS[i] = 1
-    else:
-        CS[i] = CS[i-1] + 1
-ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
-문제는 두 문자열의 길이, 공통 부분 배열의 위치가 다를 수도 있음.
+dp[i][j]에는 각 부분 문제들의 답을 기록해놓는 거고, 
+각 문자열의 다음 문자로 넘어가면 이전 문제의 답에서 시작해서 한 문자만 추가하면 되는 것.
 
-일치하는 순간 재귀로 들어가서 쭉 탐색하는 건 어떨지? 
+str1[i] == str2[j-1]일 때 대각선 위에서 +1을 해주는 이유는
+하위 문제를 점차 확장해나간다는 아이디어에선, 
+현재 상황이 양쪽 문자열에서 한 글자씩 빠진 상황에서의 답에
+양쪽 문자열에 현재 문자들 하나씩만 추가된 상황이나 마찬가지이기 때문. 
 '''
 
 
-A = input()
-B = input()
+def lcs(str1, str2):
+    dp = [[] for i in range(len(str1)+1)]
+    for i in dp:
+        for j in range(len(str2) + 1): 
+            i.append(0)
 
-na, nb = len(A), len(B)
+    for i in range(1, len(str1) + 1):
+        for j in range(1, len(str2) + 1):
+            # 현재 양쪽 문자가 일치하는 경우
+            if str1[i-1] == str2[j-1]:
+                dp[i][j] = dp[i-1][j-1] + 1 # LCS 업데이트 (대각선 위의 값 + 1)
 
-# 더 긴 문자열 기준으로 세팅
-CS = [0]*max(na, nb)
+                print(f'str1[{i-1}] = str2[{j-1}]! ({str1[i-1]}={str2[j-1]})')
+                for _ in dp:
+                    print(_)
+                print('--------------------')
+                
+            # 문자가 일치하지 않는 경우
+            else:
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]) # 왼쪽, 위쪽 중에 큰 값 물려받기
 
-if A[0] == B[0]:
-    CS[0] = 1
+    return dp[len(str1)][len(str2)]
 
-save_i = 0
-save_j = 0
-for i in range(na):
-    for j in range(save_j, nb):
-        if A[i] == B[j]:
-            CS[i] = CS[save_i] + 1
-            save_i = i
-            save_j = j+1
-            break
 
-print(max(CS))
+### I/O ### 
+str1 = input()
+str2 = input()
 
+print(lcs(str1, str2))
 
 
