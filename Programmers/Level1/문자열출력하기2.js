fa7e18b71472ffe7b542789db7dc478fa80c2e23
Vacayy <juyeong.kim.201@gmail.com>
2024-01-12 16:34:41 +0900

    240112

--- Programmers/Level1/문자열출력하기2.js
+++ Programmers/Level1/문자열출력하기2.js
@@ -1,24 +1,3 @@
-/* 
-[모듈 가져오기]
-여기서 readline 모듈을 불러오고 있습니다. 이 모듈은 Node.js에서 사용자의 입력을 읽는 데 사용됩니다
-*/
-const readline = require('readline');
-
-/*
-[readline 인터페이스 생성]
-readline.createInterface 메소드는 input과 output 스트림을 설정하여 readline 인터페이스를 생성합니다. 
-process.stdin은 표준 입력 스트림(보통 키보드 입력)을, process.stdout은 표준 출력 스트림(보통 콘솔 출력)을 나타냅니다.
-*/
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-/*
-[변수 선언]
-input이라는 이름의 빈 배열을 선언합니다. 이 배열은 나중에 사용자의 입력을 저장하는 데 사용됩니다.
-*/
-let input = [];
 
 /*
 ['line' 이벤트 리스너]
