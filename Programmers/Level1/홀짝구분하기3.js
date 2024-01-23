3439b3e005e464f5794455ed9959ddf53d6c998c
Vacayy <juyeong.kim.201@gmail.com>
2024-01-23 23:16:34 +0900

    240123

--- Programmers/Level1/홀짝구분하기3.js
+++ Programmers/Level1/홀짝구분하기3.js
@@ -13,3 +13,11 @@ rl.on('line', function (line) {
     const result = n % 2 == 0 ? 'even' : 'odd';
     console.log(`${n} is ${result}`);    
 });
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    n = Number(input[0]);
+    const result = n % 2 == 0 ? 'even' : 'odd';
+    console.log(`${n} is ${result}`);    
+});
